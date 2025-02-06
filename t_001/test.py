3d96bf60ed83969b4620f3f236f6dd390b3b0bff
qin <henuqin@gmail.com>
2025-02-06 13:46:31 +0800

    test k8s create service

--- t_001/test.py
+++ t_001/test.py
@@ -1,8 +1,14 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    inputStr = requests.args.get('inputstr','')
+    response = requests.get('https://httpbin.org/anything')
+    print('Hello, World!')
+    return {
+        response.json()
+    }
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=50004)
